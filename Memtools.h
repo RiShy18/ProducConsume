07c971fce1a12ce91ee9a3b561240ef134a6c833
Francisco Murillo <fmurillom@gmail.com>
2020-06-23 15:22:39 -0600

    [ADD] Count Produced Messages, Count Kernel Time

--- Memtools.h
+++ Memtools.h
@@ -7,12 +7,12 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-int isEmpty(Buffer *Buffer, int size);
-int isFull(Buffer *Buffer, int size);
-int isFree(Buffer *Buffer, int size);
+int isEmpty(buffer *Buffer, int size);
+int isFull(buffer *Buffer, int size);
+int isFree(buffer *Buffer, int size);
 int freeAll(int shmid);
-int getSpot(Buffer *Buffer, int processID);
-int newMem(int *shmid, key_t key, int size, Buffer **Memory);
+int getSpot(buffer *Buffer, int processID);
+int newMem(int *shmid, key_t key, int size, buffer **Memory);
 int newPack(int *shmid, Pack **Mem);
 
 
